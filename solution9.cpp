195d966964f3036fe82891b303c9a7b9879bbd29
hellfiresong <hellfiresong.me@gmail.com>
2016-02-26 11:40:58 +0800

    Palindrome number judge.

---
+++ solution9.cpp
@@ -0,0 +1,56 @@
+/**
+ * Leetcode problem 9: judge an integer is palindrome.
+ * 
+ * hellfire (asyncloading#163.com)
+ * Feb 22nd, 2016
+ */
+#include<iostream>
+using namespace std;
+
+class Solution {
+  public:
+    bool isPalindrome(int x) {
+          // negative integers
+      if( x < 0 ) {
+        return false;
+      }
+      if( x == 0 ) {
+	    return true;    
+      }
+
+      if (x == reverse(x))
+      {
+        return true;
+      }
+      else
+      {
+        return false; 
+      }                          
+    }
+
+    int reverse(int x)
+    {
+      int result = 0;
+
+      // Overflow situation
+      if (x < INT_MIN || x > INT_MAX )
+      {
+        return 0;
+      }
+
+      while (x / 10 > 0)
+      {
+        result = result * 10 + x % 10;
+        x /= 10;
+      }
+      result = result * 10 + x;
+      return result;
+    }
+    
+};
+
+int main(int argc, char **argv)
+{
+  Solution s;
+  cout << s.isPalindrome(1534334351) << endl;
+}
